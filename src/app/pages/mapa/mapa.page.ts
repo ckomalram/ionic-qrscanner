81aff49fea74f5bf8bc90887f522b6d1e60860f4
Carlyle Komalram <glaw14@gmail.com>
2022-05-21 23:19:51 -0500

    agregando cdn de mapbox

--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+declare let mapboxgl: any;
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage implements OnInit {
+export class MapaPage implements OnInit, AfterViewInit {
 
   latitud: number;
   longitud: number;
@@ -19,9 +21,17 @@ export class MapaPage implements OnInit {
 
     this.latitud = Number(geo[0]);
     this.longitud = Number(geo[1]);
-    console.log(geo);
-    console.log(this.latitud );
-    console.log(this.longitud );
+    // console.log(geo);
+    // console.log(this.latitud );
+    // console.log(this.longitud );
+  }
+
+  ngAfterViewInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2tvbWFscmFtIiwiYSI6ImNsM2dmc2cyZjAwZXYzZGs0ZjRzYmgxdTEifQ.urGL3g7hJ71B1kYwTHpn8w';
+    const  map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+    });
   }
 
 }
